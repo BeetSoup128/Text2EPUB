3d0bf83aa0a238a78150fe374dc0b9984b8f0089
BeetSoup233 <136452093+BeetSoup128@users.noreply.github.com>
2024-10-12 16:39:39 +0800

    Update !!!Run.bat
    更多功能，更改py脚本位置

--- !!!Run.bat
+++ !!!Run.bat
@@ -1,11 +1,30 @@
-:: 新建Windows Terminal tab, 使用pwsh7运行python脚本. 可以主动选择conda环境
-@echo off
+:: 新建Windows Terminal tab, 使用pwsh运行python脚本. 可以主动选择conda环境
+:: @echo off
+SET USINGFINALPROCESS=1
 SET USINGCONDA=1
-SET CONDAENV=WebR
-SET PYFILENAME=textpublisher.py
+SET USINGBACKUPDIR=1
+
+SET CONDAENV="WebR"
+SET PYFILENAME="txtPub.py"
+SET BACDIR="D:\BeetSoup\Onedrive\!Novel"
+SET SAVDIR="./!!!Backups/"
 IF %USINGCONDA% == 1 (
-  SET PSCOMMAND="{conda activate %CONDAENV%\; python ./%PYFILENAME% \; Read-Host -Prompt 'All work is done. Press Enter to close'}"
-)ELSE(
-  SET PSCOMMAND="{python ./%PYFILENAME% \; Read-Host -Prompt 'All work is done. Press Enter to close'}"
+  SET "ENTERENV=conda activate %CONDAENV:~1,-1%\;"
+) ELSE (
+  SET "ENTERENV= "
 )
+IF %USINGFINALPROCESS% == 1 (
+  SET "FTECHISTR= $iStr = Read-Host 'All work is done. Press CLEAR to remove all into `%SAVDIR%` ' \;"
+  IF %USINGBACKUPDIR% == 1 ( SET "WEBSYNC=Copy-Item -Path ./*.ePub -Destination %BACDIR% -Force \;" ) ELSE ( SET "WEBSYNC=  "   )
+  SET "SAVETXT2BAC= Move-Item -Path ./*.txt  -Destination %SAVDIR% -Force \;"
+  SET "SAVEEPUB2BAC=Move-Item -Path ./*.ePub -Destination %SAVDIR% -Force \;"
+  SET "FINAL= %FTECHISTR% if ('CLEAR'.Equals( $iStr ) ) { %WEBSYNC% %SAVETXT2BAC% %SAVETXT2BAC% } \; "
+) ELSE (
+  SET "FINAL= "
+)
+
+SET "RUNNING=python ./Utils/%PYFILENAME:~1,-1%\;"
+
+SET PSCOMMAND="{%ENTERENV% %RUNNING% %FINAL% }"
+
 wt nt pwsh -WorkingDirectory %~dp0 -c pwsh -Command %PSCOMMAND%
